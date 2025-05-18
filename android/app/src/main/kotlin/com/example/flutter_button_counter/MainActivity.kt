ce82cc0ae73c0b5bd20aca79afb6f9ada525367b
Pradap Pandiyan <pradapjackie@gmail.com>
2025-05-18 11:44:25 +0400

    updated the test

--- android/app/src/main/kotlin/com/example/flutter_button_counter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_button_counter/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.flutter_button_counter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+}
