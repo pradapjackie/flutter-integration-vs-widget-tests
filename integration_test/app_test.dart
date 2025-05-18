da04a30b528ceb0d1227a747e7a3f1ac37c7ecca
Pradap Pandiyan <pradapjackie@gmail.com>
2025-05-18 10:52:18 +0400

    updated the test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:flutter_button_counter/main.dart';
 
 void main() {
